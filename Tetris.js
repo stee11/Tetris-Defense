7236d35ae6c49906190899256f420e7beb672491
Steven Kolln <stee11ftw@gmail.com>
2013-01-13 23:32:27 -0500

    Made the code readable; title screen 100% functional. Hell yeah

--- Tetris.js
+++ Tetris.js
@@ -1,22 +1,5 @@
-/**function writeScreen(message)
-{
-	document.write(message);
-}
-
-function Position
-{
-	this.xcor;
-	this.ycor;
-	this.block;
-};
-
-var posGrid = new Array(7);
-for(int i = 0; i < 7; i++)
-{
-	posGrid[i] = new Array(14);
-}**/
-
- window.requestAnimFrame = (function(){
+//Thanks Kevin Coughlin, we "took" (stole) this from your block game!!! :D 
+window.requestAnimFrame = (function(){
       return  window.requestAnimationFrame       || 
               window.webkitRequestAnimationFrame || 
               window.mozRequestAnimationFrame    || 
@@ -26,6 +9,7 @@ for(int i = 0; i < 7; i++)
                 window.setTimeout(callback, 1000 / 60);
               };
     })();
+//These are not the global variables you are looking for
 var canvas = document.getElementById('canvas1');		
 var ctx = canvas.getContext('2d');	
 var sprites = [];
@@ -34,17 +18,17 @@ var inTitle=true;
 var titleArray =[];
 var blockArray =[];
 
+//Audio instantiation for title screen
 var title = new Audio("./Sound/Newtitle.wav");
 title.loop = true;
 title.play();
-		
+
+//Clears Canvas		
 var clearCanvas = function()
 {
-	ctx.save();
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.restore();
 }
-
+//The mute button
 var muteButton = function()
 {
 	if(title.paused)
@@ -61,11 +45,6 @@ var muteButton = function()
 	
 var init = function()
 {
-	var bgm = new Audio("Sound/bgm.mp3");
-	bgm.loop = true;
-	//bgm.play();
-	//title.play();
-
 	//block instantiation. Alphabetical order by shape
 	blockArray[0]=new Animation(new Image(), 0,0,17,17,0,0);
 	blockArray[0].image.src="./Graphics/Circle Block.gif";
@@ -80,11 +59,13 @@ var init = function()
 
 
 	//yoshi instantiation
-	var yoshiImage = new Image();
+	//This is the small yoshi that runs; most likely will not be used
+
+	/*var yoshiImage = new Image();
 	yoshiImage.src = "./Graphics/YoshiTest.png"
 	var yoshi = new Sprite(250, 400, 30, 48);
 	var runAnim = new Animation(yoshiImage, 0, 0, 30, 48, 12, 4);
-	yoshi.setAnimation(runAnim);
+	yoshi.setAnimation(runAnim);*/
 
 	//trickman instantiation
 	var trickmanImage = new Image();
@@ -126,6 +107,7 @@ var init = function()
 	//Title Instantiation
 	var logo = new Image();
 	logo.src="./Graphics/Logo.png";
+
 	//Exit Instantiation
 	var exit = new Image();
 	exit.src = "./Graphics/exit1.png"
@@ -144,15 +126,9 @@ var init = function()
 	titleArray.push(logo);
 	titleArray.push(start);
 	titleArray.push(exit);
-
-
 	
 	gameLoop();
 }
-
-////////////////////////////
-////////////////////////////
-////////////////////////////
 
 //This is the event used to capture keyevents
 //The internet told me to do it this way
@@ -164,17 +140,19 @@ var init = function()
        `'----'._\--'  
      * Whale whale whale, what have we here?
      */
+
+//The keyboard down event
 canvas.onkeydown = function(evt) {
        var charCode = evt.which;
        var charStr = String.fromCharCode(charCode);
        keyPress(charStr);
    };
 
+//The mouse click event
 canvas.onclick=function(evt) {
 	//console.log("x is: "+evt.offsetX+"\ny is: "+evt.offsetY); 
 	//Use this line above to find x and y of a click
 	mouseClick(evt);
-
 };
 
 //This function is called by the canvas.onclick. The event object is passed to it and the 
@@ -200,14 +178,13 @@ function mouseClick(MouseEvent)
 						}
 						else
 							sprites[1].y=108;
-
 				}
 			}
 		}
 	}
-	else
+	else //If not in title screen
 	{
-
+		//Will be added later
 	}
 }
 function keyPress(event)
@@ -222,90 +199,56 @@ function keyPress(event)
 				sprites[1].y=57;
 		}
 	}
-	else
+	else //If not in title screen
 	{
-
+		//Will be added later
 	}
 }
 
-////////////////////////////
-////////////////////////////
-///////////////////////////
-
 var rainingBlocks = function()
 {
-	//global
-	//init
-	//var circleBlockImage  = new Image().src="./Graphics/Circle Block.gif";
-	//circleBlockImage.src = "./Graphics/Circle Block.gif"
-
-	/*var circleBlockImage = new Image();
-	circleBlockImage.src = "./Graphics/Circle Block.gif"
-	var circleBlockAnim = new Animation(circleBlockImage, 0, 0, 17, 17, 0, 0);
-	
-	var heartBlockImage = new Image();
-	heartBlockImage.src = "./Graphics/Heart Block.gif"
-	var heartBlockAnim = new Animation(heartBlockImage, 0, 0, 17, 17, 0, 0);
-	
-	var diamondBlockImage =  new Image();
-	diamondBlockImage.src = "./Graphics/Diamond Block.gif"
-	var diamondBlockAnim = new Animation(diamondBlockImage, 0, 0, 17, 17, 0, 0);
-	
-	var starBlockImage = new Image();
-	starBlockImage.src = "./Graphics/Star Block.gif"
-	var starBlockAnim = new Animation(starBlockImage, 0, 0, 17, 17, 0, 0);
-	
-	var triangleBlockImage = new Image();
-	triangleBlockImage.src = "./Graphics/Triangle Block.gif"
-	var triangleBlockAnim = new Animation(triangleBlockImage, 0, 0, 17, 17, 0, 0);*/
-
-	var numBlocks = Math.floor(Math.random()*0+ 1);
+	var numBlocks = Math.floor(Math.random()*2+ 1);
 	for(var i = 0; i < numBlocks; i++)
 	{
-		//window.innerWidth is the size of the window width
-		var randX = Math.floor(Math.random()*window.innerWidth -26);
-		//var newSprite = new Sprite(randX, 0, 17, 17);
+		var randX = Math.floor(Math.random()*canvas.width-17);
 		var blockType = Math.floor(Math.random()*5);
-		//blockType=0;
-		//console.log(new Sprite(randX,0,17,17).setAnimation(blockArray[0]))
+		sprites.push(new Sprite(randX,0,17,17));
 		switch(blockType)
 		{
 			case 0:
-				sprites.push(new Sprite(randX,0,17,17));
 				sprites[sprites.length-1].setAnimation(blockArray[0]);
-				//newSprite.setAnimation(circleBlockAnim);
-				//console.log(newSprite);
 				break;
 			case 1:
-				sprites.push(new Sprite(randX,0,17,17));
 				sprites[sprites.length-1].setAnimation(blockArray[1]);
 				break;
 			case 2:
-				sprites.push(new Sprite(randX,0,17,17));
 				sprites[sprites.length-1].setAnimation(blockArray[2]);
 				break;
 			case 3:
-				sprites.push(new Sprite(randX,0,17,17));
 				sprites[sprites.length-1].setAnimation(blockArray[3]);
 				break;
 			case 4:
-				sprites.push(new Sprite(randX,0,17,17));
 				sprites[sprites.length-1].setAnimation(blockArray[4]);
 				break;
 		}
-		console.log(sprites[sprites.length-1]);
-		sprites[sprites.length-1].draw();
-		//sprites.push(newSprite);
 	}
+	for(var i = sprites.length-1; i > 4; i--) //5 is the first block in the sprite array
+		{
+			sprites[i].draw(ctx);
+			if(sprites[i].getY() > canvas.height)
+			{
+				sprites.splice(i, 1); //get rid of the block and tag for garbage collection
+			}
+			else
+			{
+				sprites[i].move(0, 4); //Move it
+			}
+		}		
 }
 
 var titleScreen = function(title, start, exit)
 {
-	//ctx.fillStyle = "black";
-	//ctx.fillRect(0, 0, canvas.width, canvas.height);
-	//ctx.fillStyle = "white";
-	//ctx.font = "italic 20pt /Fonts/AGENCYB.ttf";
-	//ctx.fillText("Tetris Defense!", 90, 35);
+	// Animation for yoshi moving up and down
 	if(frameCounter > 50)
 	{
 		sprites[0].move(0, -0.4);
@@ -329,98 +272,62 @@ var titleScreen = function(title, start, exit)
 		sprites[0].setAnimation(TitleYoshiStill);
 		frameCounter = 0;
 	}
-	//ctx.drawImage(start, window.innerWidth/4, window.innerWidth/6, 366, 85);
-	//ctx.drawImage(exit, 0, 0, window.innerWidth/2.5, window.innerHeight*(window.innerHeight*(5/6)));
+	//adds a counter to frame number
 	frameCounter++;
-	ctx.drawImage(exit, 90, 100, 60, 40);
-	ctx.drawImage(title, 90,10);
-	ctx.drawImage(start, 90, 50, 60, 40);
-	sprites[0].draw(ctx);
+	//draws the title screen exit, start, and logo images
+	ctx.drawImage(titleArray[2], 90, 100, 60, 40); //exit
+	ctx.drawImage(titleArray[0], 90,10); //title
+	ctx.drawImage(titleArray[1], 90, 50, 60, 40); //start
+	//draws yoshi going up and down
+	sprites[0].draw(ctx); //title yoshi
 
+	//animation for the arrow moving left and right
 	if(frameCounter >= 0 && frameCounter <= 13)
 	{
-		//console.log("first called")
 		sprites[1].move(-0.8, 0);
 	}
 	else if (frameCounter > 13 && frameCounter <= 26)
 	{
-		//console.log("second called")
 		sprites[1].move(0.8, 0);
 	}
 	else if(frameCounter <= 39 && frameCounter > 26)
 	{
-		//console.log("third called")
 		sprites[1].move(-.8,0);
 	}
 	else if(frameCounter >39 && frameCounter <= 52)
 	{
-		//console.log("fourth called")
 		sprites[1].move(0.8, 0);
 	}
 	else if (frameCounter >52 && frameCounter <= 65)
 	{
-		//console.log("fifth called")
 		sprites[1].move(-.8,0);
 	}
 	else if (frameCounter >65 && frameCounter <= 78)
 	{
-		//console.log("sixth called")
 		sprites[1].move(.8, 0);
 	}
 	else if(frameCounter <= 91 && frameCounter > 78)
 	{
-		//console.log("seventh called")
 		sprites[1].move(-.8,0);
 	}
 	else if(frameCounter > 91 && frameCounter <=104)
 	{
-		//console.log("eigth called")
 		sprites[1].move(0.8, 0);
 	}
-
 }
 
 var gameLoop = function ()
 {
-	rainingBlocks();
 	clearCanvas();
-	//ctx.drawImage(background, 0, 0, window.innerWidth, window.innerHeight);
 	ctx.fillStyle = "black";
 	console.log(sprites.length);
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-		for(var i = sprites.length-1; i >= 0; i--)
-		{
-			//this.sprites[i].draw(ctx);
-			if(i == 1)
-			{
-				this.sprites[i].draw(ctx);
-			}
-			else
-			{
-				if(i > 4)
-				{
-					sprites[i].draw(ctx);
-					//console.log(canvas.height)
-					if(this.sprites[i].getY() > canvas.height)
-					{
-						//console.log("yolo");
-						this.sprites.splice(sprites.length-1, 1);
-					}
-					else
-					{
-						this.sprites[i].move(0, 2);
-					}
-
-					this.sprites[i].move(0, 2);
-				}
-			}
-		}
-		if (inTitle)
-		{
-			//console.log("called");	
-			titleScreen(titleArray[0], titleArray[1], titleArray[2]);
-		}	
-		requestAnimFrame(gameLoop, canvas);
+	rainingBlocks();
+	if (inTitle)
+	{
+		sprites[1].draw(ctx); //the arrow	
+		titleScreen(titleArray[0], titleArray[1], titleArray[2]);
+	}	
+	requestAnimFrame(gameLoop, canvas);
 }	
 	
-
